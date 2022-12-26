ae0c8cd44b3d146e8d66f3f3f6dacc1ca0b2c20f
Swapnali <swapnali.kajale25@gmail.com>
2022-12-26 11:53:54 +0530

    Architect Assian development

--- src/test/java/qaclickacademy/RestAPITest.java
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -16,4 +16,10 @@ public class RestAPITest {
 	{
 		System.out.println("deleteTwitter");	
 	}
+
+	@Test
+	public void deleteTwitter12()
+	{
+		System.out.println("deleteTwitter");	
+	}
 }
--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -15,5 +15,10 @@ public class SeleniumTest {
 		{
 			System.out.println("elementsUi");	
 		}
+		@Test
+		public void elementsU1i()
+		{
+			System.out.println("elementsUi");	
+		}
 		
 }
